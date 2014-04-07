bcc30a7be172e4e108881ca4947639b35ac24ca1
Draconvolk <davidchapman05@gmail.com>
2014-04-07 10:43:01 -0400

    Initial Upload
    This contains the DLL with the commands to be sent to the USB missile
    launcher, the USBLib.dll used to communicate with the missile launcher,
    and a simple console application that uses these dlls to send the
    commands to the missile launcher.

---
+++ Missiles/MissileConsole/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MissileCommand;
+using System.Threading;
+
+namespace MissileConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var command = "";
+			bool ledState = false;
+			Console.WriteLine("Missile Defense Online... ");
+			while (command.ToLower() != "quit" && command.ToLower() != "exit")
+			{
+				Console.WriteLine(">>");
+				command = Console.ReadLine();
+				ExecuteCommand(command, ref ledState);
+
+			}
+		}
+
+		public static void ExecuteCommand(string command, ref bool ledState){
+
+			MissileLauncher missilecommand = new MissileLauncher();
+			switch (command.ToLower())
+			{
+				case "up":
+					missilecommand.Up();
+					Thread.Sleep(20);
+					missilecommand.Stop();
+					break;
+				case "down":
+					missilecommand.Down();
+					Thread.Sleep(20);
+					missilecommand.Stop();
+					break;
+				case "left":
+					missilecommand.Left();
+					Thread.Sleep(40);
+					missilecommand.Stop();
+					break;
+				case "right":
+					missilecommand.Right();
+					Thread.Sleep(40);
+					missilecommand.Stop();
+					break;
+				case "fire":
+
+					missilecommand.Fire();
+					//Thread.Sleep(2000);
+					//missilecommand.Stop();
+					break;
+				case "status":
+					missilecommand.Get_Status();
+					break;
+				case "stop":
+					missilecommand.Stop();
+					break;
+				case "led":
+					if (ledState)
+						missilecommand.LED_off();
+					else
+						missilecommand.LED_on();
+					ledState = !ledState;
+					break;
+				case "led_on":
+					missilecommand.LED_on();
+					break;
+				case "led_off":
+					missilecommand.LED_off();
+					break;
+				case "quit":
+					
+					break;
+				default: Console.WriteLine("Command '{0}' not found.", command);
+					break;
+			}
+			
+		}
+	}
+}
---
+++ Missiles/MissileLauncher/MissileLauncher.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsbLibrary;
+
+namespace MissileCommand
+{
+
+	public class MissileCommands
+	{
+		public byte[] Up() { return new byte[10] { 0, 2, 2, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] Down() { return new byte[10] { 0, 2, 1, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] Left() { return new byte[10] { 0, 2, 4, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] Right() { return new byte[10] { 0, 2, 8, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] Fire() { return new byte[10] { 0, 2, 16, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] Stop() { return new byte[10] { 0, 2, 32, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] Get_Status() { return new byte[10] { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] LED_On() { return new byte[10] { 0, 3, 1, 0, 0, 0, 0, 0, 0, 0 }; }
+		public byte[] LED_Off() { return new byte[10] { 0, 3, 0, 0, 0, 0, 0, 0, 0, 0 }; }
+		public MissileCommands() { }
+
+	}
+
+    public class MissileLauncher
+    {
+		public MissileLauncher()
+		{
+			USBInit();
+
+		}
+
+		public MissileCommands commands = new MissileCommands();
+
+		private UsbHidPort USB;
+
+		private void USBInit()
+		{
+			USB = new UsbHidPort();
+
+			USB.VID_List[0] = 2689;
+			USB.PID_List[0] = 1793;
+			USB.VID_List[1] = 8483;
+			USB.PID_List[1] = 4112;
+			USB.ID_List_Cnt = 2;
+			IntPtr handle = new IntPtr(1508034);
+			USB.RegisterHandle(handle);
+		}
+
+		private void SendUSBData(byte[] Data)
+		{
+			if (this.USB.SpecifiedDevice != null)
+			{
+				try
+				{
+					this.USB.SpecifiedDevice.SendData(Data);
+				}
+				catch (Exception ex)
+				{
+					throw ex;
+				}
+			}
+		}
+
+		public void Up()
+		{
+			SendUSBData(commands.Up());
+		}
+
+		public void Down()
+		{
+			SendUSBData(commands.Down());
+		}
+
+		public void Left()
+		{
+			SendUSBData(commands.Left());
+		}
+
+		public void Right()
+		{
+			SendUSBData(commands.Right());
+		}
+
+		public void Fire()
+		{
+			SendUSBData(commands.Fire());
+		}
+
+		public void Stop()
+		{
+			SendUSBData(commands.Stop());
+		}
+
+		public void LED_on()
+		{
+			SendUSBData(commands.LED_On());
+		}
+
+		public void LED_off()
+		{
+			SendUSBData(commands.LED_Off());
+		}
+
+		public void Get_Status()
+		{
+			SendUSBData(commands.Get_Status());
+		}
+    }
+}
